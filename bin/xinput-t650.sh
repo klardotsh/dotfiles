a2e2432f2bb810f4a9aa997725e3b79495105578
Josh Klar <j@iv597.com>
2016-08-11 14:27:51 -0500

    Finally have a linux-friendly trackpad, sheesh

---
+++ bin/xinput-t650.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+export DISPLAY=":0"
+
+xinput set-prop "Logitech Rechargeable Touchpad T650" "libinput Tapping Enabled" 1
+xinput set-prop "Logitech Rechargeable Touchpad T650" "libinput Natural Scrolling Enabled" 1
