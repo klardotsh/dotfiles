648a755214ebec5040b6fa2de18af2f55fdf19cc
Josh Klar <j@iv597.com>
2018-07-16 17:29:26 -0700

    Resolve systemctl freezes by just locking bar to internal display. Way nicer this way anyway.

--- .screenlayout/internal.sh
+++ .screenlayout/internal.sh
@@ -1,4 +1,3 @@
 export DISPLAY=:0
 
 xrandr --output DP-1 --off --output DP-2 --off
-systemctl --user restart polybar
--- .screenlayout/work.sh
+++ .screenlayout/work.sh
@@ -1,10 +1,4 @@
 export DISPLAY=:0
 
-systemctl --user stop polybar
-
 xrandr --output eDP-1 --auto
 xrandr --output DP-2 --auto --right-of eDP-1
-
-sleep 1
-
-systemctl --user start polybar
