c7a1accd83e5497732747d87f6ec35c0697e010b
Josh Klar <josh@klar.sh>
2019-11-08 22:28:00 -0800

    Sway as a service manager I guess?

--- bin/sway_exit.sh
+++ bin/sway_exit.sh
@@ -3,10 +3,14 @@
 if [ -x /bin/systemctl ]; then
 	systemctl --user stop waybar
 	systemctl --user stop mako
-else
+elif [ -x /bin/sv ]; then
 	SVDIR=${XDG_CONFIG_HOME}/sv sv stop waybar
 	SVDIR=${XDG_CONFIG_HOME}/sv sv stop mako
 	SVDIR=${XDG_CONFIG_HOME}/sv sv stop swaybg
+else
+	pulseaudio -k
+	killall waybar
+	killall mako
 fi
 
 swaymsg exit
--- bin/sway_services.sh
+++ bin/sway_services.sh
@@ -5,8 +5,13 @@ export > ${XDG_RUNTIME_DIR}/sway_services.env
 if [ -x /bin/systemctl ]; then
 	systemctl --user start waybar
 	systemctl --user start mako
-else
+elif [ -x /bin/sv ]; then
 	SVDIR=${XDG_CONFIG_HOME}/sv sv start swaybg
 	SVDIR=${XDG_CONFIG_HOME}/sv sv start waybar
 	SVDIR=${XDG_CONFIG_HOME}/sv sv start mako
+else
+	mako &
+	jack_control start
+	pulseaudio -d && sleep 1
+	waybar &
 fi
