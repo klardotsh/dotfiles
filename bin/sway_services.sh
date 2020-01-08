955008c7695f475664185c4a4790f29d48bffef2
Josh Klar <josh@klar.sh>
2020-01-08 01:55:30 -0800

    chore: more sane hostname-based sway configs; hostname rename

--- bin/sway_services.sh
+++ bin/sway_services.sh
@@ -6,7 +6,6 @@ if [ -x /bin/systemctl ]; then
 	systemctl --user start waybar
 	systemctl --user start mako
 elif [ -x /bin/sv ]; then
-	SVDIR=${XDG_CONFIG_HOME}/sv sv start swaybg
 	SVDIR=${XDG_CONFIG_HOME}/sv sv start waybar
 	SVDIR=${XDG_CONFIG_HOME}/sv sv start mako
 else
