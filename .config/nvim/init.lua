f76ef0a1cb240fc8872a3881121368863f4ffc90
Josh Klar <josh@klar.sh>
2023-12-28 08:49:47 -0800

    actually configure cmdline, cool plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -138,6 +138,33 @@ require('jetpack.packer').add {
 	'editorconfig/editorconfig-vim', -- force buffer to use editorconfig settings
 	'ntpeters/vim-better-whitespace',
 	'vim-scripts/DeleteTrailingWhitespace',
+	{'vzze/cmdline.nvim',
+	config = function()
+		require('cmdline')({
+			window = {
+				matchFuzzy = true,
+				offset     = 1, -- depending on 'cmdheight' you might need to offset
+				debounceMs = 10
+			},
+
+			hl = {
+				default   = "Pmenu",
+				selection = "PmenuSel",
+				directory = "Directory",
+				substr    = "LineNr"
+			},
+
+			column = {
+				maxNumber = 6,
+				minWidth  = 20
+			},
+
+			binds = {
+				next = "<Tab>",
+				back = "<S-Tab>"
+			}
+		})
+	end},
 
 	{'lewis6991/gitsigns.nvim',
 	config = function()
