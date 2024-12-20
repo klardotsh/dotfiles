bd2cf18b0c6fa4902d41c24ff8a438fc23580e36
Josh Klar <josh@klar.sh>
2024-12-20 14:54:42 -0800

    fonts: Iosevka Extended with various ligatures everywhere; wezterm config fanciness for changing curated fonts.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,6 +3,11 @@ local wezterm = require 'wezterm'
 local DARK_THEME = 'duskfox'
 local LIGHT_THEME = 'dawnfox'
 
+local FP_Iosevka = 'iosevka'
+local FP_Maple = 'maple'
+
+local FONT_PRESET = FP_Iosevka
+
 -- Thanks, https://github.com/wez/wezterm/issues/4681#issuecomment-2320537074
 local function get_cursor_theme()
 	local success, stdout, stderr =
@@ -27,35 +32,59 @@ local function light_dark_toggle(window, _)
 	window:set_config_overrides({ color_scheme = newScheme })
 end
 
+local function config_font_for_preset(preset)
+	if preset == FP_Iosevka then
+		primary_font = { family = 'Iosevka Term', stretch = 'Expanded', weight = 'Regular' }
+	elseif preset == FP_Maple then
+		primary_font = 'Maple Mono'
+	end
+
+	return wezterm.font_with_fallback({
+		primary_font,
+		'FSD Emoji',
+		'Symbols Nerd Font',
+	})
+end
+
+
+local function config_harfbuzz_for_preset(preset)
+	local hb = {
+		'calt=1', 'liga=1', -- Generically turn ligatures on
+	}
+
+	if preset == FP_Iosevka then
+		table.insert(hb, 'dlig=0') -- Explicitly disable due to awful [| and friends
+		table.insert(hb, 'ss14=1') -- PragmataPro style
+	elseif preset == FP_Maple then
+		table.insert(hb, 'dlig=1')
+		table.insert(hb, 'cv35=1') -- Disable cursive-italic lowercase L
+
+		-- Enable all feature sets in Maple Mono
+		table.insert(hb, 'ss01=1')
+		table.insert(hb, 'ss02=1')
+		table.insert(hb, 'ss03=1')
+		table.insert(hb, 'ss04=1')
+		table.insert(hb, 'ss05=1')
+		table.insert(hb, 'ss06=1')
+		table.insert(hb, 'ss07=1')
+		table.insert(hb, 'ss08=1')
+		table.insert(hb, 'ss09=1')
+
+		table.insert(hb, 'zero=1') -- Dotted zero
+	end
+
+	return hb
+end
+
 return {
 	-- Default to dark theme to avoid flashbangs on new windows at night; use keybind to
 	-- toggle to light mode after launch.
 	color_scheme = DARK_THEME,
 	default_cursor_style = 'BlinkingBar',
 	enable_tab_bar = false,
-	font = wezterm.font_with_fallback({
-		'Maple Mono',
-		'FSD Emoji',
-		'Symbols Nerd Font',
-	}),
+	font = config_font_for_preset(FONT_PRESET),
 	font_size = 15.0,
-	harfbuzz_features = {
-		'zero=1',
-		'calt=1',
-		'dlig=1',
-		'clig=1',
-		'liga=1',
-		'ss01=1',
-		'ss02=1',
-		'ss03=1',
-		'ss04=1',
-		'ss05=1',
-		'ss06=1',
-		'ss07=1',
-		'ss08=1',
-		'ss09=1',
-		'cv35=1',
-	},
+	harfbuzz_features = config_harfbuzz_for_preset(FONT_PRESET),
 
 	keys = {
 		{ key = "o", mods = "ALT", action = wezterm.action_callback(light_dark_toggle) },
