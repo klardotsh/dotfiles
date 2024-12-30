272520a03a6c3919f8a0882146b4d6d0836da9e7
Josh Klar <josh@klar.sh>
2024-12-29 18:57:10 -0800

    fish: Trying this shell out, so start tracking its configs.

---
+++ .config/fish/config.fish
@@ -0,0 +1,171 @@
+set -U fish_greeting
+
+set __OS "$(uname -s)"
+if [ $(lsb_release -si 2>&1 || echo 'n/a') = 'VoidLinux' ]
+	set IS_VOID true
+end
+
+# Also set system-wide, but better safe than sorry.
+set -x LANG en_US.utf8
+
+set -x GPG_TTY "$(tty)"
+set -x SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/gnupg/S.gpg-agent.ssh"
+
+if type -q wezterm
+	set -x TERMINAL wezterm
+end
+
+if type -q nvim
+	set -x EDITOR nvim
+else
+	set -x EDITOR vi
+end
+
+if type -q bat
+	set -x PAGER "bat --style=plain"
+	set -x MANPAGER "$PAGER -l man"
+end
+
+set -x LESS "-R"
+
+set -x AWS_SDK_LOAD_CONFIG true
+set -x BAT_THEME ansi
+set -x FZF_DEFAULT_OPTS "--color=16 --extended"
+
+set -x TFENV_PATH ~/.tfenv/bin
+
+set -x NODE_COMPILE_CACHE ~/.cache/nodejs-compile-cache
+set -x NPM_PACKAGES ~/.npm-packages
+fish_add_path "$NPM_PACKAGES/bin"
+
+set -x GOPATH ~/.go
+fish_add_path "$GOPATH/bin"
+
+if type -q rustc
+	set -x RUST_SRC_PATH "$(rustc --print sysroot)/lib/rustlib/src/rust/src"
+end
+fish_add_path ~/.cargo/bin
+
+if type -q ruby
+	fish_add_path "$(ruby -e 'print Gem.user_dir')/bin"
+end
+
+if type -q podman
+	set -x OCI_RUNNER podman
+else
+	set -x OCI_RUNNER docker
+end
+
+set -x BROWSER xdg-open
+set -x MOZ_USE_XINPUT2 1
+set -x MOZ_ENABLE_WAYLAND 1
+set -x MOZ_WAYLAND_USE_VAAPI 1
+
+set -x GTK_CSD 0
+set -x GTK2_RC_FILES ~/.gtkrc-2.0
+set -x QT_QPA_PLATFORM wayland-egl
+
+set -x BEMENU_BACKEND wayland
+set -x CLUTTER_BACKEND wayland
+set -x SDL_VIDEODRIVER "wayland,x11"
+
+set -x XDG_CURRENT_DESKTOP sway
+set -x XDG_SESSION_TYPE wayland # otherwise gets set to 'tty', breaking WebRTC things
+
+set -x _JAVA_OPTIONS '-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
+set -x _JAVA_AWT_WM_NONREPARTENTING 1
+
+if status is-interactive
+	alias e $EDITOR
+	alias m mosh
+	alias mux "tmux.sh $(hostname)"
+
+	alias todo "todo.sh -d ~/.todo/todo.cfg"
+	alias t todo
+	alias tbr "t ls +tbr"
+
+	if $IS_VOID
+		set -x PRIV_CHANGER doas
+		alias sudo $PRIV_CHANGER
+
+		alias pf "xbps-query -Rs"
+		alias pff "xlocate"
+		alias pffi "xlocate -S"
+		alias pi "$PRIV_CHANGER xbps-install -S"
+		alias pql "xbps-query -f"
+		alias pqs "xbps-query -s"
+		alias pu "$PRIV_CHANGER sh -c 'xbps-install -Su && xbps-remove -yOoR && vkpurge rm all'"
+	else
+		set -x PRIV_CHANGER sudo
+		alias sudo "sudo -E "
+	end
+
+	alias nmap-quickscan "$PRIV_CHANGER nmap -sV -T4 -O -F --version-light"
+
+	if type -q bat
+		alias cat bat
+	end
+
+	alias cp "cp -iv"
+	alias mv "mv -iv"
+	alias rm "rm -iv"
+
+	alias chmod "chmod -c"
+	alias chown "chown -c"
+
+	alias ip="ip -c"
+
+	if type -q rg
+		alias grep rg
+		alias egrep "rg -e"
+		alias frg "rg --files-with-matches"
+		alias fgrep frg
+	end
+
+	if type -q eza
+		alias ls eza
+		alias tree "eza -T"
+	end
+
+	if type -q colordiff
+		alias diff "colordiff -Nuar"
+	else
+		alias diff "diff -Nuar"
+	end
+
+	if type -q qrencode
+		alias quickqr "qrencode -t ansiutf8"
+	end
+
+	# HTTPie replacement
+	if type -q xh
+		alias http xh
+	end
+
+	alias gl "git lol"
+
+	alias gls "git lol --since '2 weeks' --author 'Josh Klar'"
+	alias gca "git cram" # Muscle memory dies hard - this is NOT git commit -a
+
+	if type -q cyme
+		alias lsusb 'cyme --lsusb'
+	end
+
+	# This section is reserved for things that start up fast enough to not add
+	# noticeable latency to shell startup - otherwise, dump to
+	# ~/.config/fish/functions!
+	fzf --fish | source
+	zoxide init fish | source
+	direnv hook fish | source
+
+	# `thefuck --alias` was dumped into ~/.config/fish/functions/fuck.fish,
+	# because Python's startup time is much, much slower than is acceptable
+	# to have in the hot path here.
+	#
+	# For cases when you override a command and turn it into an alias (e.g.
+	# alias sed=gsed or alias git=hub), you should add all them to the
+	# THEFUCK_OVERRIDDEN_ALIASES environment variable, separated by comma. For
+	# example, in your config.fish:
+	#
+	# set -x THEFUCK_OVERRIDDEN_ALIASES 'gsed,git'
+end
